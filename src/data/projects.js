00d9fef7d207e235c6be8e50daf65e405c1d67ba
Milly <liuhongmein@gmail.com>
2023-11-06 21:35:50 -0500

    projects tags color

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,38 +2,6 @@ import { v4 as uuidv4 } from 'uuid';
 
 const imgUrl = 'https://placehold.co/600x400';
 
-const colors = [
-  'bg-slate-300',
-  'bg-gray-300',
-  'bg-red-300',
-  'bg-orange-300',
-  'bg-amber-300',
-  'bg-yellow-300',
-  'bg-lime-300',
-  'bg-green-300',
-  'bg-emerald-300',
-  'bg-teal-300',
-  'bg-cyan-300',
-  'bg-sky-300',
-  'bg-blue-300',
-  'bg-indigo-300',
-  'bg-violet-300',
-  'bg-purple-300',
-  'bg-fuchsia-300',
-  'bg-pink-300',
-  'bg-rose-300',
-];
-
-const tagColors = {
-  JavaScript: 'bg-slate-400',
-  React: 'bg-red-300',
-  Tailwindcss: 'bg-orange-300',
-};
-
-export function getTagColor(tag) {
-  return tagColors[tag];
-}
-
 export const projects = [
   {
     id: uuidv4(),
@@ -93,3 +61,35 @@ const tags = [
   ),
 ];
 console.log(tags);
+
+const colors = [
+  'bg-slate-300',
+  'bg-gray-300',
+  'bg-red-300',
+  'bg-orange-300',
+  'bg-amber-300',
+  'bg-yellow-300',
+  'bg-lime-300',
+  'bg-green-300',
+  'bg-emerald-300',
+  'bg-teal-300',
+  'bg-cyan-300',
+  'bg-sky-300',
+  'bg-blue-300',
+  'bg-indigo-300',
+  'bg-violet-300',
+  'bg-purple-300',
+  'bg-fuchsia-300',
+  'bg-pink-300',
+  'bg-rose-300',
+];
+
+const tagColors = {};
+for (let i = 0; i < tags.length; i++) {
+  tagColors[tags[i]] = colors[i];
+}
+console.log(tagColors);
+
+export function getTagColor(tag) {
+  return tagColors[tag];
+}
