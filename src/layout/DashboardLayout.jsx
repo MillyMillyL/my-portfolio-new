d25f0a77ab22689cd882b3086c3da31129de3e13
Milly <liuhongmein@gmail.com>
2024-01-25 20:00:29 -0500

    fix

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import DashSideBar from '../components/dashSideBar';
+import DashSideBar from '../components/DashSideBar';
 
 function DashboardLayout() {
   return (
